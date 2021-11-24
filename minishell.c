61ac9b39cea6f880620fddba129c128e9c99b655
Alexandre DE TEMMERMAN <ade-temm@e1r1p7.42.fr>
2021-11-24 17:35:39 +0100

    commentary about print env upfated

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: ade-temm <ade-temm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 11:26:48 by adylewsk          #+#    #+#             */
-/*   Updated: 2021/11/24 17:34:21 by ade-temm         ###   ########.fr       */
+/*   Updated: 2021/11/24 17:35:19 by ade-temm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	interpret_command(char *command)
 	printf("la commande : %s\n", command);
 }
  //clear history : rl_clear_history();
- //print l'env : printf("%s\n", *g_env);
+ //print l'env : printf("%s\n", *(env->g_env));
 
 int		main(int ac, char **av, char **envp)
 {
@@ -29,7 +29,6 @@ int		main(int ac, char **av, char **envp)
 	using_history();
 	env = (t_env *)malloc(sizeof(t_env));
 	env->g_env = envp;
-	printf("%s\n", *(env->g_env));
 	while (1)
 	{
 		command = readline("Minishell $> ");
